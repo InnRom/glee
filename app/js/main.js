ef63ebdb8d4587f4cef645489ca59ee18a167e82
InnRom <ieromanof@gmail.com>
2022-04-20 09:46:21 +0700

    added section design

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,18 @@ autoplaySpeed: 2000
 
 });
 
- var mixer = mixitup(".product-items");
+
+ var containerEl1 = document.querySelector('[data-ref="container-1"]');
+ var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+ 
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
+
 
 });
