5297413649fe9418b35c8a0bba95680d3125c266
InnRom <ieromanof@gmail.com>
2022-05-25 16:55:56 +0700

    added product details page

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,34 @@
 $(function(){
 
 
-$('.slider-top__inner').slick({
-  dots:true,
-  arrows:false,
-  fade:true,
-  autoplay:true,
-  autoplaySpeed: 2000
+
+  $('.products-slide__thumb').slick({
+    asNavFor: '.products-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable:false,  
+    arrows:false,
+  });
+  $('.products-slide__big').slick({
+    asNavFor: '.products-slide__thumb',
+    draggable:false,
+    arrows:false,
+    fade:true,
+    
+  });
 
 
-});
+  $('.slider-top__inner').slick({
+    dots:true,
+    arrows:false,
+    fade:true,
+    autoplay:true,
+    autoplaySpeed: 2000
+
+
+  });
 
 
  var containerEl1 = document.querySelector('[data-ref="container-1"]');
@@ -31,6 +50,8 @@ $('.slider-top__inner').slick({
 });
 
 $(function(){
+
+$('.select-style, .products-content__num').styler();
 
   $(".filter-price__input").ionRangeSlider({
     type: "double", 
@@ -56,12 +77,29 @@ $(function(){
     spacing: "5px"
   });
 
-$(".catalog-content__star").rateYo({
+  $(".catalog-content__star").rateYo({
     starWidth: "17px",
     normalFill: "#d6d6d6",
     ratedFill: "#ffcc00",
     readOnly: true,
     spacing: "10px"
   });
+
+  $(".products-content__star").rateYo({
+    starWidth: "17px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing: "10px"
+
+})
+
+$('.products-relate__slide').slick({
+  lazyLoad: 'ondemand',
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  // autoplay:true,
+  // arrows:false,
+});
 
 });
